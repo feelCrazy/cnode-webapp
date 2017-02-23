b7d7a25de062e88ddf26810bcd7a47a6b915e1aa
CRwming <btming217@gmail.com>
2017-02-23 16:46:30 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './assets/logo.svg';
 import './assets/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Welcome to React</h2>
+                    </div>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
